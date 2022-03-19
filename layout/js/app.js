6dbc2b03a5e9af458741616bb7f84aa7fb34bad8
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-03-19 21:11:00 +0300

    commit init

--- layout/js/app.js
+++ layout/js/app.js
@@ -59,15 +59,15 @@ ${heroData.map(function(item){
             <div class="hr1 bg-border position-absolute"></div>
             <div class="hr2 bg-border position-absolute"></div>
             <div class="hero-contact position-absolute">
-                <a class="btn">${item.a}</a>
-                <h1>${item.h1}</h1>
-                <h1>${item.h2}</h1>
-                <h1>${item.h3}</h1>
-                <p class="cat"><i class="fa fa-wpforms"></i><span>${item.more}</span></p>
+                <a class="btn fw bg-danger text-white fs-4">${item.a}</a>
+                <h1 class="lh fw fs-1 text-white">${item.h1}</h1>
+                <h1 class="lh fw fs-1 text-white">${item.h2}</h1>
+                <h1 class="lh fw fs-1 text-white">${item.h3}</h1>
+                <p class="cat text-uppercase text-white fs-3"><i class="fa fa-wpforms"></i><span>${item.more}</span></p>
             </div>
-                <img src="${item.image}" alt="" class="hero-img">
+                <img src="${item.image}" alt="" class="hero-img position-absolute">
             
-            <h1 class="number position-absolute">${item.number}</h1>
+            <h1 class="number z-1 position-absolute text-white">${item.number}</h1>
         </div>
     `
 }).join('')}`
@@ -100,9 +100,9 @@ document.getElementById("all-items").innerHTML = `
 ${sectionData.map(function(item){
     return`
 <div class="items position-relative">
-    <p class="date"><span>${item.name}</span>/${item.date}</p>
-    <h1 class="text-sacound">${item.descri}</h1>
-    <p class="cat"><span>${item.more}</span></p>
+    <p class="date fw"><span class="text-danger">${item.name}</span>/${item.date}</p>
+    <h1 class="text-sacound fs-2">${item.descri}</h1>
+    <p class="cat"><i class="fa fa-wpforms"></i> <span>${item.more}</span></p>
     <div class="efict position-absolute text-border fw">${item.num}</div>
 </div>
 `
@@ -110,7 +110,72 @@ ${sectionData.map(function(item){
 console.log(item.more)
 }).join('')}`;
 
+const footerDate =[
+    {
+        name:   "news",
+        p1:     "all", 
+        p2:     "club news", 
+        p3:     "media center", 
+        p4:     "photos", 
+        p5:     "video", 
+        p6:     "rss",
+    },
+    {
+        name:   "tickets",
+        p1:     "tickets", 
+        p2:     "online ticket siiop", 
+        p3:     "vouchers", 
+        p4:     "combi-ticket", 
+        p5:     "payment", 
+        p6:     "contact and booking"
+    },
+    {
+        name:   "matches",
+        p1:     "standings", 
+        p2:     "fixtures", 
+        p3:     "luga ", 
+        p4:     "poush cup", 
+        p5:     "poush super cup", 
+        p6:     ""
+    },
+    {
+        name:   "fans",
+        p1:     "fan clubs", 
+        p2:     "social media", 
+        p3:     "arena tour", 
+        p4:     "matchday tour", 
+        p5:     "online", 
+        p6:     ""
+    },
+    {
+        name:   "business newsletter mobile app",
+        p1:     "", 
+        p2:     "", 
+        p3:     "", 
+        p4:     "", 
+        p5:     "", 
+        p6:     ""
+    }
+];
 
+document.getElementById("row").innerHTML = `
+${footerDate.map(function(item){
+    return`
+        <div class="card d-flex align-items-start flex-column">
+            <div class="card-header">
+                <h1 class="fs-3 text-sacound text-uppercase fw">${item.name}</h1>
+            </div>
+            <div class="card-body">
+                <p class="text-ccc fs-4 text-uppercase">${item.p1}</p>
+                <p class="text-ccc fs-4 text-uppercase">${item.p2}</p>
+                <p class="text-ccc fs-4 text-uppercase">${item.p3}</p>
+                <p class="text-ccc fs-4 text-uppercase">${item.p4}</p>
+                <p class="text-ccc fs-4 text-uppercase">${item.p5}</p>
+                <p class="text-ccc fs-4 text-uppercase">${item.p6}</p>
+            </div>
+        </div>
+`
+}).join('')}`;
 
 var sliderHero = Array.from(document.querySelectorAll(".hero .hero-info"));
 
